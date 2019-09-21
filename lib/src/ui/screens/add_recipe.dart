a9143efa97e933db237ffe294ba9afad5efd14e2
Tabi Idris <tabidrizy@gmail.com>
2019-09-21 09:47:57 +0100

    complete ui for add recipes

--- lib/src/ui/screens/add_recipe.dart
+++ lib/src/ui/screens/add_recipe.dart
@@ -14,6 +14,9 @@ class _AddRecipeState extends State<AddRecipe> {
   final _formKey = GlobalKey();
   File galleryFile;
 
+  List<String> ingredients = [];
+  List<String> preparation = [];
+
   // TextEditingController _controller = TextEditingController();
 
   final db = Firestore.instance;
@@ -43,79 +46,188 @@ class _AddRecipeState extends State<AddRecipe> {
     }
 
     return Scaffold(
-        appBar: AppBar(
-            title: Text("Add A Recipe",
-                style: TextStyle(fontSize: 30, color: Colors.orange))),
-        body: Container(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-            child: Builder(
-                builder: (context) => Form(
-                    key: _formKey,
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          TextFormField(
-                            decoration: InputDecoration(
-                               icon: const Icon(Icons.edit),
-                               labelText: 'Type'
-                            ),
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return 'Please enter recipe type';
-                              }
+      appBar: AppBar(
+          title: Text("Add A Recipe",
+              style: TextStyle(fontSize: 30, color: Colors.orange))),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+        child: Builder(
+          builder: (context) => Form(
+            key: _formKey,
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      icon: const Icon(Icons.edit), labelText: 'Type'),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter recipe type';
+                    }
+                  },
+                  // onSaved: (val) =>
+                  //     setState(() => _user.firstName = val),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      icon: const Icon(Icons.mode_edit), labelText: 'Name'),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter recipe name.';
+                    }
+                  },
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      icon: const Icon(Icons.timer), labelText: 'Duration'),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter recipe Duration.';
+                    }
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Ingredients',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center),
+                ),
+                Column(
+                  children: ingredients.reversed.map((item) {
+                    return Dismissible(
+                      key: Key(item),
+                      onDismissed: (DismissDirection direction) {
+                        setState(() {
+                           ingredients.remove(item);
+                        });
+                      },
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.edit),
+                          labelText: 'Enter ingredient',
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.remove),
+                            onPressed: () {
+                              debugPrint('222');
                             },
-                            // onSaved: (val) =>
-                            //     setState(() => _user.firstName = val),
                           ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              icon: const Icon(Icons.mode_edit),
-                              labelText: 'Name'
-                            ),
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return 'Please enter recipe name.';
-                              }
-                            },
-                          ),
-                          TextFormField(
-                            keyboardType: TextInputType.number,
-                            decoration: InputDecoration(
-                              icon: const Icon(Icons.timer),
-                              labelText: 'Duration'
-                            ),
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return 'Please enter recipe Duration.';
-                              }
-                            },
-                          ),
-                          displaySelectedFile(galleryFile),
-                          RaisedButton(
-                            child: new Text('Select Recipe Image'),
-                            onPressed: imageSelectorGallery,
-                          ),
-                          // onSaved: (val) =>
-                          //     setState(() => _user.lastName = val)),
-                          // Container(
-                          //     padding: const EdgeInsets.symmetric(
-                          //         vertical: 16.0, horizontal: 16.0),
-                          //     child: RaisedButton(
-                          //         onPressed: () {
-                          //           final form = _formKey.currentState;
-                          //           if (form.validate()) {
-                          //             form.save();
-                          //             _user.save();
-                          //             _showDialog(context);
-                          //           }
-                          //         },
-                          //         child: Text('Save'))),
-                        ])))));
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        RaisedButton(
+                          color: Colors.red,
+                          child: new Text('Clear All',
+                              style: TextStyle(color: Colors.white)),
+                          onPressed: () {
+                            setState(() {
+                              ingredients.clear();
+                            });
+                          },
+                        ),
+                        RaisedButton(
+                          color: Colors.green,
+                          child: new Text('Add Ingredient',
+                              style: TextStyle(color: Colors.white)),
+                          onPressed: () {
+                            setState(() {
+                              ingredients.add('');
+                            });
+                          },
+                        ),
+                      ]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Preparation',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center),
+                ),
+                Column(
+                  children: preparation.reversed.map((item) {
+                    return Dismissible(
+                        key: Key(item),
+                        onDismissed: (DismissDirection direction) {
+                          setState(() {
+                             preparation.remove(item);
+                          });
+                        },
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.edit),
+                              labelText: 'Enter preparatory step',
+                              suffixIcon: IconButton(
+                                  icon: Icon(Icons.remove),
+                                  onPressed: () {
+                                    debugPrint('222');
+                                  })),
+                        ));
+                  }).toList(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        RaisedButton(
+                          color: Colors.red,
+                          child: new Text('Clear All',
+                              style: TextStyle(color: Colors.white)),
+                          onPressed: () {
+                            setState(() {
+                              preparation.clear();
+                            });
+                          },
+                        ),
+                        RaisedButton(
+                          color: Colors.green,
+                          child: new Text('Add Preparation',
+                              style: TextStyle(color: Colors.white)),
+                          onPressed: () {
+                            setState(() {
+                              preparation.add('');
+                            });
+                          },
+                        ),
+                      ]),
+                ),
+                displaySelectedFile(galleryFile),
+                RaisedButton(
+                  child: new Text('Select Recipe Image'),
+                  onPressed: imageSelectorGallery,
+                ),
+                // onSaved: (val) =>
+                //     setState(() => _user.lastName = val)),
+                // Container(
+                //     padding: const EdgeInsets.symmetric(
+                //         vertical: 16.0, horizontal: 16.0),
+                //     child: RaisedButton(
+                //         onPressed: () {
+                //           final form = _formKey.currentState;
+                //           if (form.validate()) {
+                //             form.save();
+                //             _user.save();
+                //             _showDialog(context);
+                //           }
+                //         },
+                //         child: Text('Save'))),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
-  _showDialog(BuildContext context) {
-    Scaffold.of(context)
-        .showSnackBar(SnackBar(content: Text('Submitting form')));
-  }
+  // _showDialog(BuildContext context) {
+  //   Scaffold.of(context)
+  //       .showSnackBar(SnackBar(content: Text('Submitting form')));
+  // }
 }
